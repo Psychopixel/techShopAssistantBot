afe8417b1f7dc4189f459392821a42a0bb925815
Psychopixel <124247646+Psychopixel@users.noreply.github.com>
2023-07-08 20:01:13 +0200

    minor change

--- robSpeak.py
+++ robSpeak.py
@@ -1,4 +1,4 @@
-version="<!#FV> 0.0.1 </#FV>"
+version="<!#FV> 0.0.2 </#FV>"
 import wave
 import pyaudio
 import azure.cognitiveservices.speech as speechsdk
@@ -118,9 +118,6 @@ def stopRecording():
     else:
         print("Nothing went wrong")
     bot = True
-    #chat_row ={"agent":"User", "text":response["text"]+"\n\n"}
-    #chat.append(chat_row)
-    #updateScreen(chat)
     recording = False
     p.terminate()
     return response
